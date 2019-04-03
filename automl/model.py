835e7837f57d3420c183fa536e91e69208c19e62
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-04-03 23:00:18 +0900

    Create requirements.txt

--- automl/model.py
+++ automl/model.py
@@ -2,7 +2,7 @@ import os
 from typing import Any
 from typing import Dict
 
-os.system('pip install numpy pandas')
+os.system('pip install -r requirements.txt')
 
 import numpy as np
 import pandas as pd
