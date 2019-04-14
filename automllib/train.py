81733988185039fac482a4697d006bc67fb7629f
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-04-15 02:06:00 +0900

    Remove hyperparameter_search

--- automllib/train.py
+++ automllib/train.py
@@ -1,8 +1,5 @@
 import logging
 
-from typing import Any
-from typing import Dict
-
 import lightgbm as lgb
 import optuna
 import pandas as pd
@@ -31,26 +28,16 @@ def train(X: pd.DataFrame, y: pd.Series) -> lgb.LGBMClassifier:
         random_state=0
     )
 
-    best_params = hyperparameter_search(clf, X, y)
-
-    clf.set_params(**best_params)
-
-    clf.fit(X, y)
-
-    return clf
-
-
-@timeit
-def hyperparameter_search(
-    clf: lgb.LGBMClassifier,
-    X: pd.DataFrame,
-    y: pd.Series
-) -> Dict[str, Any]:
+    sampler = optuna.samplers.TPESampler(seed=0)
+    study = optuna.create_study(sampler=sampler)
     objective = Objective(clf, X, y, cv=3)
-    study = optuna.create_study()
 
     study.optimize(objective, n_trials=10)
 
     logger.info(f"ROCAUC = {-study.best_value:.3f}")
 
-    return study.best_params
+    clf.set_params(**study.best_params)
+
+    clf.fit(X, y)
+
+    return clf
