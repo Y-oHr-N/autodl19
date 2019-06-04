7e0b7f86bad9cf5d8099ae0aa18cc031cb5a5573
i146m <13403169+i146m@users.noreply.github.com>
2019-06-04 12:08:55 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import os
 
-# os.system('pip3 install -q colorlog')
-# os.system('pip3 install -q imbalanced-learn')
-# os.system('pip3 install -q lightgbm')
-# os.system('pip3 install -q optuna')
-# os.system('pip3 install -q pandas==0.24.2')
-# os.system('pip3 install -q scikit-learn>=0.21.0')
+os.system('pip3 install -q colorlog')
+os.system('pip3 install -q imbalanced-learn')
+os.system('pip3 install -q lightgbm')
+os.system('pip3 install -q optuna')
+os.system('pip3 install -q pandas==0.24.2')
+os.system('pip3 install -q scikit-learn>=0.21.0')
 
 import pandas as pd
 
