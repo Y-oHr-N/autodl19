096f6409da06828c53a92e822234bb900304f29a
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-07-13 21:03:24 +0900

    Fix ensemble.py

--- automllib/ensemble.py
+++ automllib/ensemble.py
@@ -144,7 +144,7 @@ class Objective(object):
                 trial.suggest_loguniform(
                     'reg_lambda',
                     self._reg_lambda_low,
-                    self._reg_alpha_high
+                    self._reg_lambda_high
                 ),
             'subsample':
                 trial.suggest_uniform(
@@ -393,7 +393,7 @@ class BaseLGBMModelCV(BaseEstimator):
         logger.info(f'Best iteration: {self.best_iteration_}.')
         logger.info(f'Best {params["metric"]}: {self.best_score_:.3f}.')
 
-        if self.n_iter_no_change is None:
+        if self.best_iteration_ is None:
             n_estimators = self.n_estimators
         else:
             n_estimators = self.best_iteration_
