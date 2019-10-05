26fd5ef9ba1ba2f50043bcfb1fb0aab3a6f47600
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-10-05 20:23:37 +0900

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,6 @@ from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.layers import Dropout
 from tensorflow.python.keras.layers import Flatten
 from tensorflow.python.keras.layers import MaxPooling2D
-from tensorflow.python.keras import models
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.preprocessing import sequence
 
@@ -44,10 +43,6 @@ def pad_seq(data, pad_len):
     )
 
 
-def ohe2cat(label):
-    return np.argmax(label, axis=1)
-
-
 def cnn_model(input_shape, num_class, max_layer_num=5):
     model = Sequential()
     min_size = min(input_shape[:2])
@@ -78,34 +73,32 @@ def cnn_model(input_shape, num_class, max_layer_num=5):
 
 
 class Model(object):
-    def __init__(self, metadata, train_output_path="./", test_input_path="./"):
+    def __init__(self, metadata):
         self.done_training = False
         self.metadata = metadata
-        self.train_output_path = train_output_path
-        self.test_input_path = test_input_path
 
     def train(self, train_dataset, remaining_time_budget=None):
         train_x, train_y = train_dataset
 
         fea_x = extract_mfcc(train_x)
-        max_len = max([len(_) for _ in fea_x])
-        fea_x = pad_seq(fea_x, max_len)
+        self.max_len = max([len(_) for _ in fea_x])
+        fea_x = pad_seq(fea_x, self.max_len)
 
         num_class = self.metadata['class_num']
         X = fea_x[:, :, :, np.newaxis]
         y = train_y
 
-        model = cnn_model(X.shape[1:], num_class)
+        self.model = cnn_model(X.shape[1:], num_class)
 
         optimizer = tf.keras.optimizers.SGD(lr=0.01, decay=1e-06)
 
-        model.compile(
+        self.model.compile(
             loss='sparse_categorical_crossentropy',
             optimizer=optimizer,
             metrics=['accuracy']
         )
 
-        model.summary()
+        self.model.summary()
 
         callbacks = [
             tf.keras.callbacks.EarlyStopping(
@@ -113,9 +106,10 @@ class Model(object):
                 patience=10
             )
         ]
-        history = model.fit(
+
+        self.model.fit(
             X,
-            ohe2cat(y),
+            np.argmax(y, axis=1),
             epochs=100,
             callbacks=callbacks,
             validation_split=0.1,
@@ -124,33 +118,11 @@ class Model(object):
             shuffle=True
         )
 
-        model.save(self.train_output_path + '/model.h5')
-
-        with open(self.train_output_path + '/feature.config', 'wb') as f:
-            f.write(str(max_len).encode())
-            f.close()
-
         self.done_training = True
 
     def test(self, test_x, remaining_time_budget=None):
-        model = models.load_model(self.test_input_path + '/model.h5')
-
-        with open(self.test_input_path + '/feature.config', 'r') as f:
-            max_len = int(f.read().strip())
-
-            f.close()
-
         fea_x = extract_mfcc(test_x)
-        fea_x = pad_seq(fea_x, max_len)
+        fea_x = pad_seq(fea_x, self.max_len)
         test_x = fea_x[:, :, :, np.newaxis]
 
-        y_pred = model.predict_classes(test_x)
-
-        test_num = self.metadata['test_num']
-        class_num = self.metadata['class_num']
-        y_test = np.zeros([test_num, class_num])
-
-        for idx, y in enumerate(y_pred):
-            y_test[idx][y] = 1
-
-        return y_test
+        return self.model.predict_proba(test_x)
